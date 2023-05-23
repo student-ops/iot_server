79276db5d7c76a39631a73d1b2486b655f0cbce0
student-ops <gogakuec@gmail.com>
2023-05-23 19:22:44 +0900

    add send message section to main.go

--- url_manage/main.go
+++ url_manage/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 type Tunnel struct {
@@ -32,10 +34,43 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-
+	var url string
 	if len(data.Tunnels) > 0 {
 		fmt.Println(data.Tunnels[0].PublicURL)
+		url = data.Tunnels[0].PublicURL
 	} else {
 		fmt.Println("No tunnels found")
 	}
+
+	slackBotOAuth := os.Getenv("SLACK_BOT_OAUTH")
+
+	json_data := map[string]interface{}{
+		"channel": "#url_manage",
+		"text":    "*Grafana:* \n"+url,
+	}
+
+	payload, err := json.Marshal(json_data)
+	if err != nil {
+		fmt.Println("Error encoding JSON:", err)
+		return
+	}
+
+	req, err := http.NewRequest("POST",  "https://slack.com/api/chat.postMessage", bytes.NewBuffer(payload))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	req.Header.Set("Authorization", "Bearer "+slackBotOAuth)
+
+	client := &http.Client{}
+	resp, err = client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("Response:", resp.Status)
 }
