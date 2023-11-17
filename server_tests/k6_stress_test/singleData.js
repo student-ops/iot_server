7cd758f730d8340cc25d4d2e719eb347b94ec7ba
student-ops <gogakuec@gmail.com>
2023-11-17 14:09:50 +0900

    update for garafana volume

--- server_tests/k6_stress_test/singleData.js
+++ server_tests/k6_stress_test/singleData.js
@@ -1,9 +1,10 @@
+import { sleep } from 'k6';
 import http from 'k6/http';
 
 export default function () {
     const url = 'http://localhost:8080/handle';
 
-    let tmp = 20 + 3 - 6 * Math.random();
+    let tmp = 20 + 5 - 10 * Math.random();
     let moi = 60 + 5 - 10 * Math.random();
     let air = 1024 + 5 - 10 * Math.random();
 
